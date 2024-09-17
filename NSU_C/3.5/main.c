20e7bca01b00dc7c4da3ea97810c1b5538f2f54c
Alexander <114004814+BlackTaze@users.noreply.github.com>
2024-09-17 20:49:03 +0700

    Update main.c

--- NSU_C/3.5/main.c
+++ NSU_C/3.5/main.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
-int main()
-{
 
+
+int main() {
+    double sum = 0;
+    for (int i = 0 ; i < 10;i++ ){
+        sum += (3.14/10) * (sin(3.14/10 * (i + 0.5)));
+    }
+    printf("%f",sum);
 }
