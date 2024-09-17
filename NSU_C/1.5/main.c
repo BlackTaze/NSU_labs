02a1240ed1d475ca6e6943cc5f7a83dfeb4a04f4
Alexander <114004814+BlackTaze@users.noreply.github.com>
2024-09-17 19:57:06 +0700

    Update main.c

--- NSU_C/1.5/main.c
+++ NSU_C/1.5/main.c
@@ -8,7 +8,7 @@ int main()
     min = (y % 30) * 2;
     x = 6 * min;
     hours = y / 30;
-    printf("%d %d \n",hours,mins);
+    printf("%d %d \n",hours,min);
     printf("%d",x);
     return 0;
 }
