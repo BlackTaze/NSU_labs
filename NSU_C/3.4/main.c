bc25a85c0f4fea734e40e3b02a984169115f2ddc
Alexander <114004814+BlackTaze@users.noreply.github.com>
2024-09-23 23:41:14 +0700

    Update main.c

--- NSU_C/3.4/main.c
+++ NSU_C/3.4/main.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 int main()
 {
     float res = 0.0;
-    for (int i = 50; i > 0; i--) res = (res + i) * 0.5;
+    for (int i = 50; i > 0; i--) res = pow((res + i) , 0.5);
     printf("%f",res);
 }
