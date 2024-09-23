f354810d488679cfbd5977768430ced1d50c1709
Alexander <114004814+BlackTaze@users.noreply.github.com>
2024-09-23 23:52:14 +0700

    Update main.c

--- NSU_C/2.3/main.c
+++ NSU_C/2.3/main.c
@@ -7,7 +7,7 @@ int main()
 
     scanf("%d%d%d%d",&a,&b,&c,&d);
     dlin = (a / c) * (b / d);
-    korot = ( b/ c) * (a / d);
+    korot = (b / c) * (a / d);
     if (dlin > korot) puts("dlin");
     else puts("korot");
 
