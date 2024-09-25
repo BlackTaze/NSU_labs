4c459a29e9f0346315bba0f6a6e940fe2a0a4d3f
Alexander <114004814+BlackTaze@users.noreply.github.com>
2024-09-25 11:09:17 +0700

    Update main.c

--- NSU_C/3.1/main.c
+++ NSU_C/3.1/main.c
@@ -4,6 +4,9 @@
 int main()
 {
     int sum = 20 * 20;
-    for ( int i = 20; i > 0; --i) sum -= i * i;
+    for ( int i = 20; i > 0; --i){
+        sum -= i;
+        sum *= sum;
+    }
     printf("%d",sum);
 }
